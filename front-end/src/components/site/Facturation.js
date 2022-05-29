c4afd2d510eb816855bbaef90732c3da20151cbf
Akahiko Yukimura <akahikoyukimura39@gmail.com>
2022-05-29 18:36:40 +0100

    add facturation print button

--- front-end/src/components/site/Facturation.js
+++ front-end/src/components/site/Facturation.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import { useReactToPrint } from "react-to-print";
+import logo from "./../../images/agrikit.png";
 
 function Facturation() {
   //get user from local storage-------------------------------------------
@@ -13,115 +15,129 @@ function Facturation() {
   const date = `${current.getDate()}/${
     current.getMonth() + 1
   }/${current.getFullYear()}`;
+
+  //print functionality------------------------------------------------------
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
   return (
     <>
       <div className="card facturation">
-        <div className="card-header bg-black"></div>
-        <div className="card-body">
-          <div className="container">
-            <div className="row">
-              <div className="col-xl-12 facturation-heading">
-                <div className="div-logo-fac">
-                  <div className="card-heading facturation-logo"></div>
+        <div ref={componentRef}>
+          <div className="card-header bg-black"></div>
+          <div className="card-body">
+            <div className="container">
+              <div className="row">
+                <div className="col-xl-12 facturation-heading">
+                  
+                    <img className="facturation-logo" src={logo} />
+                  
+                  <ul className="list-unstyled float-end">
+                    <li style={{ fontSize: "30px", color: "#26953c" }}>
+                      AgriKit
+                    </li>
+                    <li>Av. Allal Al Fassi, Rabat</li>
+                    <li>06 32 13 14 65</li>
+                    <li>elmourtazakkawtar39@gmail.com</li>
+                  </ul>
                 </div>
-                <ul className="list-unstyled float-end">
-                  <li style={{ fontSize: "30px", color: "#26953c" }}>
-                    AgriKit
-                  </li>
-                  <li>Av. Allal Al Fassi, Rabat</li>
-                  <li>06 32 13 14 65</li>
-                  <li>elmourtazakkawtar39@gmail.com</li>
-                </ul>
               </div>
-            </div>
 
-            <div className="row text-center">
-              <h3
-                className="text-uppercase text-center mt-3"
-                style={{ fontSize: "40px" }}
-              >
-                Invoice
-              </h3>
-            </div>
-            <hr></hr>
-            <div className="row mx-3">
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th scope="col">Description</th>
-                    <th scope="col">Amount</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {customerData.customerCart.carts.map((produc) => {
-                    return (
-                      <>
-                        <tr>
-                          <td>{produc.pName}</td>
-                          <td className="span-quantity-fac">
-                            {produc.pPrice * produc.pQuantity} ${" "}
-                            <span className="fac-span">{produc.pQuantity}</span>
-                          </td>
-                        </tr>
-                      </>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-            <div className="row">
-              <div className="col-xl-8">
-                <ul className="list-unstyled float-end me-0">
-                  <li>
-                    <span className="me-3 float-start">Total Amount:</span>
-                    {customerData.customerCart.totalPrice} $
-                  </li>
-                  <li>
-                    {" "}
-                    <span className="me-5">Discount:</span>0 $
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <hr></hr>
-            <div className="row">
-              <div className="col-xl-12 facturation-heading">
-                <ul className="list-unstyled float-end">
-                  <li style={{ fontSize: "30px", color: "#26953c" }}>
-                    Mr. {customerData.customerDetails.customerName}
-                  </li>
-                  <li>
-                    Address: {customerData.customerDetails.customerAddress}{" "}
-                  </li>
-                  <li>Email: {user.uEmail}</li>
-                </ul>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-xl-8">
-                <p
-                  className="float-end"
-                  style={{
-                    fontSize: "30px",
-                    color: "red",
-                    fontWeight: "400",
-                    fontFamily: "Arial, Helvetica, sans-serif",
-                  }}
+              <div className="row text-center">
+                <h3
+                  className="text-uppercase text-center mt-3"
+                  style={{ fontSize: "40px" }}
                 >
-                  Total:
-                  <span>{customerData.customerCart.totalPrice} $</span>
+                  Invoice
+                </h3>
+              </div>
+              <hr></hr>
+              <div className="row mx-3">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Description</th>
+                      <th scope="col">Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {customerData.customerCart.carts.map((produc) => {
+                      return (
+                        <>
+                          <tr>
+                            <td>{produc.pName}</td>
+                            <td className="span-quantity-fac">
+                              {produc.pPrice * produc.pQuantity} ${" "}
+                              <span className="fac-span">
+                                {produc.pQuantity}
+                              </span>
+                            </td>
+                          </tr>
+                        </>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+              <div className="row">
+                <div className="col-xl-8">
+                  <ul className="list-unstyled float-end me-0">
+                    <li>
+                      <span className="me-3 float-start">Total Amount:</span>
+                      {customerData.customerCart.totalPrice} $
+                    </li>
+                    <li>
+                      {" "}
+                      <span className="me-5">Discount:</span>0 $
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <hr></hr>
+              <div className="row">
+                <div className="col-xl-12 facturation-heading">
+                  <ul className="list-unstyled float-end">
+                    <li style={{ fontSize: "30px", color: "#26953c" }}>
+                      Mr. {customerData.customerDetails.customerName}
+                    </li>
+                    <li>
+                      Address: {customerData.customerDetails.customerAddress}{" "}
+                    </li>
+                    <li>Email: {user.uEmail}</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-xl-8">
+                  <p
+                    className="float-end"
+                    style={{
+                      fontSize: "30px",
+                      color: "red",
+                      fontWeight: "400",
+                      fontFamily: "Arial, Helvetica, sans-serif",
+                    }}
+                  >
+                    Total:
+                    <span>{customerData.customerCart.totalPrice} $</span>
+                  </p>
+                </div>
+              </div>
+
+              <div className="row mt-2 mb-5">
+                <p className="fw-bold">
+                  Date: <span className="text-muted">{date}</span>
                 </p>
               </div>
             </div>
-
-            <div className="row mt-2 mb-5">
-              <p className="fw-bold">
-                Date: <span className="text-muted">{date}</span>
-              </p>
-            </div>
           </div>
+          <div className="card-footer bg-black"></div>
         </div>
-        <div className="card-footer bg-black"></div>
+        <span onClick={handlePrint} className=" text-center btn-fac">
+          Print this out!
+        </span>
       </div>
     </>
   );
