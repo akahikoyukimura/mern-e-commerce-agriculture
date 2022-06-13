f8ab3c90dac389191dac19768b6f4e2423050680
Akahiko Yukimura <akahikoyukimura39@gmail.com>
2022-06-13 02:30:26 +0100

    deploy12

--- front-end/src/components/Api.js
+++ front-end/src/components/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  //baseURL: `https://secure-test-web.herokuapp.com/`
-  baseURL: `https://mern-e-commerce-project.herokuapp.com/`
+  //baseURL: `https://mern-e-commerce-project.herokuapp.com/`
+  baseURL: `http://localhost:5000/`
 });
